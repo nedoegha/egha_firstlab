3dfbaeddab20864bc213be765863947ede399110
nedoegha <147260744+nedoegha@users.noreply.github.com>
2023-10-07 22:42:23 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,42 @@
+$(function () {
+	$(window).scroll(function() {
+	    $('#learn .section-title').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInDown");
+	        }
+	    });
+	});
+    $(window).scroll(function() {
+	    $('#learn .skills').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInUp");
+	        }
+	    });
+	});
+    $(window).scroll(function() {
+	    $('#form .section-title').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInDown");
+	        }
+	    });
+	});
+    $(window).scroll(function() {
+	    $('#form .form-btn').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("bounceIn");
+	        }
+	    });
+	});
+})
